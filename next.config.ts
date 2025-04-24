fc48e5cf4b8ae55b6e044b1bf433d558f1cdd9e6
Oluwaleke richard seyi <oluwalekerichardisaac@gmail.com>
2025-04-24 17:56:00 +0100

    Fix (Conflict) Fix On Next Image

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'd23exngyjlavgo.cloudfront.net',
-      'adds-token-info-29a861f.s3.eu-central-1.amazonaws.com',
-      'adds-token-info-29a861f.s3.amazonaws.com',
-      'logo.moralis.io',
-      'entities-logos.s3.amazonaws.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // allow all hosts (not recommended for performance and security)
+      },
     ],
   },
+  // images: {
+  //   domains: [
+  //     'd23exngyjlavgo.cloudfront.net',
+  //     'adds-token-info-29a861f.s3.eu-central-1.amazonaws.com',
+  //     'adds-token-info-29a861f.s3.amazonaws.com',
+  //     'logo.moralis.io',
+  //     'entities-logos.s3.amazonaws.com'
+  //   ],
+  // },
 };
 
 export default nextConfig;
